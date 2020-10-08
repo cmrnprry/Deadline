38b33699e05768812214327f29a12096ce482942
cmrnprry <perry.ca@husky.neu.edu>
2020-10-08 11:35:34 -0400

    can switch between cameras, although probably not the best code :(

---
+++ Deadline/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GameState
+{
+    ScreenOne, ScreenTwo, Walking
+}
+
+
+public class GameManager : MonoBehaviour
+{
+    // Keeps track of the player game state
+    public static GameState state = GameState.ScreenOne;
+
+    //If true, the player was last looking at screenOne
+    //If false, the player was last looking at screenTwo
+    public bool isScreenOne;
+    public ScreenManager screenManager;
+
+    private void Start()
+    {
+        StartCoroutine(WaitingForScreenChange());
+    }
+
+    // Update is called once the state is changed
+    void UpdateGameState()
+    {
+        switch (state)
+        {
+            case GameState.ScreenOne:
+                isScreenOne = true;
+                screenManager.ChangeCameraView(isScreenOne);
+                break;
+
+            case GameState.ScreenTwo:
+                isScreenOne = false;
+                screenManager.ChangeCameraView(isScreenOne);
+                break;
+
+            case GameState.Walking:
+                Debug.Log("The player is now standing");
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    //Waits until the button to change the screens appears then changes it
+    IEnumerator WaitingForScreenChange()
+    {
+        yield return new WaitUntil(() => Input.GetButtonDown("Switch Screens"));
+
+        _ = state == GameState.ScreenOne ? state = GameState.ScreenTwo : state = GameState.ScreenOne;
+        UpdateGameState();
+
+        yield return new WaitForEndOfFrame();
+
+        StartCoroutine(WaitingForScreenChange());
+    }
+}
---
+++ Deadline/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScreenManager : MonoBehaviour
+{
+    [Header("Cameras")]
+    public Camera one;
+    public Camera two;
+
+    public void ChangeCameraView(bool isScreenOne)
+    {
+        one.enabled = isScreenOne;
+        two.enabled = !isScreenOne;
+    }
+}
