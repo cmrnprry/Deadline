1171bbb73597767b5e2a1262de20bfdd456c09f6
Kinsey <kinsmerick@gmail.com>
2020-10-16 20:07:07 -0400

    Created TextFileParser and EssayManager
    TextFileParser takes a text file (that has the entire essay in it) and parses it for a key character that indicates BREAKS and when research is needed (default character is ">"). It breaks the essay up into sections according to this symbol and has a public method that can be used to get the section at a given index value.
    
    The EssayManager can truncate an essay section into subsections according to a given # of characters per keystroke. Then, if the player can type and is on the essay screen, they can press any input except for keys that are otherwise used and it updates a TMPro asset subsection by subsection until it reaches the end of the subsections, at which point research is needed.
    
    *Currently* no signal is sent elsewhere to indicate that the end of a section has been reached. It also has the check for correct game state commented out because the game state requires a game manager with proper connections to many other managers that I don't have set up in my test scene and I don't want to mess with Sam's!

---
+++ Deadline/Assets/Scripts/UI/EssayManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EssayManager : MonoBehaviour
+{
+    public TextMeshProUGUI essayTMPro;
+    public int charactersPerKeystroke;
+
+    private GameManager _gm;
+    private TextFileParser _tfp;
+
+    private string _currentSection;
+    private List<string> _subsections = new List<string>();
+
+    private int _essaySectionIndex = 0;
+    private int _intraSectionProgressIndex = 0;
+
+    private bool _canYouWriteMore = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_gm = FindObjectOfType<GameManager>();
+        _tfp = FindObjectOfType<TextFileParser>();
+
+        _currentSection = _tfp.GetEssaySection(0);
+
+        if(_currentSection != null)
+        {
+            TruncateSection(_currentSection);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*  if (_gm.isScreenOne && _canYouWriteMore)
+          {
+              if (CheckForInput())
+              {
+                  Type();
+              }
+          }*/
+
+        if (_canYouWriteMore)
+        {
+            if (CheckForInput())
+            {
+                Type();
+            }
+        }
+    }
+
+    /*Clears the subsections list. Checks to see if the given section is evenly divided by the desired number of characters per keystroke and stores
+     the total amount of subsections that can be made from this section accordingly. Then, it loops through the section, adding the number of subsections to
+    the list.*/
+    private void TruncateSection(string section)
+    {
+        _subsections.Clear();
+
+        int length = section.Length;
+        int subsections;
+
+        if(length % charactersPerKeystroke == 0)
+        {
+            subsections = length / charactersPerKeystroke;
+        }
+        else
+        {
+            subsections = (length / charactersPerKeystroke) + 1;
+        }
+        
+        for(int i = 0; i < subsections; i++)
+        {
+            if(i != subsections - 1)
+            {
+                _subsections.Add(section.Substring(0, charactersPerKeystroke));
+                section = section.Remove(0, charactersPerKeystroke);
+            }
+            else
+            {
+                _subsections.Add(section);
+            }
+        }
+    }
+
+    //Checks to see if the player inputs a key input that isn't hotkeyed elsewhere.
+    private bool CheckForInput()
+    {
+        if(Input.anyKeyDown && (!Input.GetButtonDown("Standing") ||
+                                !Input.GetButtonDown("Switch Screens") ||
+                                !Input.GetButtonDown("Pause") ||
+                                !Input.GetMouseButtonDown(0) ||
+                                !Input.GetMouseButtonDown(1)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //Adds the next subsection to the essay TMPro. If that's the last subsection, it sets _canYouWriteMore to false
+    //and resets the _intraSectionProgressIndex to zero.
+    private void Type()
+    {
+        essayTMPro.text += _subsections[_intraSectionProgressIndex];
+        _intraSectionProgressIndex++;
+        Debug.Log(_intraSectionProgressIndex);
+
+        if(_intraSectionProgressIndex >= _subsections.Count)
+        {
+            _canYouWriteMore = false;
+            _intraSectionProgressIndex = 0;
+        }
+    }
+}
---
+++ Deadline/Assets/Scripts/UI/TextFileParser.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TextFileParser : MonoBehaviour
+{
+    public TextAsset fullEssayTextFile;
+    public string signalBreakCharacter = ">";
+
+    private string _essayText;
+    private List<string> _essaySections = new List<string>();
+
+
+    private void Awake()
+    {
+        _essayText = fullEssayTextFile.text;
+
+        if (signalBreakCharacter == null)
+        {
+            signalBreakCharacter = ">";
+        }
+
+        ParseText(_essayText, signalBreakCharacter);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /*Loops through a text  looking for a specific break key. If found, it adds everything up to the first index of that break key to a list of strings
+     * called _essaySections and then updates the string by removing that substring. If the break key is not found, the while loop breaks and it checks if
+     * the text still contains a character, as we can assume that this is the last part of the essay and thus there is no lasting break key afterwards,
+     * so all of this string is added as the last section in _essaySections.*/
+
+    private void ParseText (string text, string breakKey)
+    {
+        while(text.Contains(breakKey))
+        {
+            int index = text.IndexOf(breakKey);
+            _essaySections.Add(text.Substring(0, index));
+            text = text.Remove(0, index + 1);
+        }
+
+        if(text.Length > 0)
+        {
+            _essaySections.Add(text);
+        }
+    }
+
+    //Returns the string of the essay section at the given index.
+    public string GetEssaySection (int index)
+    {
+        if(_essaySections[index] != null)
+        {
+            return _essaySections[index];
+        }
+        else
+        {
+            Debug.LogWarning("There is no essay section that exists at index " + index);
+            return null;
+        }
+    }
+}
